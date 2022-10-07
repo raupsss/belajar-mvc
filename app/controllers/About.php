5812bbe42407d19931507a97c2a53c979f860a8b
raupsss <androghanz@gmail.com>
2022-10-07 15:24:13 +0700

    menambahkan bootstrap

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -3,15 +3,19 @@ class About extends Controller
 {
     public function index($nama = 'Raup', $pekerjaan = 'programmer', $umur = 18)
     {
+        $data['judul'] = 'About';
         $data['nama'] = $nama;
         $data['pekerjaan'] = $pekerjaan;
         $data['umur'] = $umur;
-        $this->view('templates/header');
+        $this->view('templates/header', $data);
         $this->view('about/index', $data);
         $this->view('templates/footer');
     }
     public function page()
     {
+        $data['judul'] = 'Page';
+        $this->view('templates/header', $data);
         $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
