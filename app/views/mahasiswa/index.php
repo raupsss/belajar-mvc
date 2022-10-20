be92de6bda0db0579e20aeeeba0bbd5d61b438bd
raupsss <androghanz@gmail.com>
2022-10-20 14:27:10 +0700

    done #insertData

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,11 +1,17 @@
 <div class="container mt-5">
     <div class="row">
         <div class="col">
-            <h3>Daftar Mahasiswa</h3>
-            <form class="form-inline">
-                <input class="form-control mr-sm-2 " type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
+            <div class="d-flex justify-content-between">
+                <h3>Data Mahasiswa</h3>
+                <form class="form-inline">
+                    <input class="form-control mr-sm-2 " type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+                <!-- Button trigger Modal -->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                    Tambah Data
+                </button>
+            </div>
             <div class="d-flex justify-content-between mt-5 flex-wrap">
 
                 <?php
@@ -21,6 +27,51 @@
                         <li class="list-group-item"><?= $mhs['jurusan']; ?></li>
                     </ul>
                 <?php endforeach; ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+                    <div class="form-group">
+                        <label for="nama">Nama Lengkap</label>
+                        <input type="text" class="form-control" id="nama" name="nama">
+                    </div>
+                    <div class="form-group">
+                        <label for="nim">NIM</label>
+                        <input type="number" class="form-control" id="nim" name="nim">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" name="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="jurusan">Jurusan</label>
+                        <select class="form-control" id="jurusan" name="jurusan">
+                            <option value="Teknik Komputer Jaringan">Teknik Komputer Jaringan</option>
+                            <option value="Sistem Informasi">Sistem Informasi</option>
+                            <option value="Rekayasa Perangkat Lunak">Rekayasa Perangkat Lunak</option>
+                            <option value="Multimedia">Multimedia</option>
+                        </select>
+                    </div>
+
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Tambah Data</button>
+                </form>
             </div>
         </div>
     </div>
